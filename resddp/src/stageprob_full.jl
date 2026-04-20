fb64a2465adef0a3852aeef0093aaa8dc2e0a346
kinevinningland <kine.v@me.com>
2026-04-20 20:05:45 +0200

    Describe change

--- resddp/src/stageprob_full.jl
+++ resddp/src/stageprob_full.jl
@@ -177,11 +177,15 @@ module StageProbFull
          #Oppreguleringskrav
          @constraint(M, reserve_req_up[z=1:NZ-1, k=1:NK],
          cap_zone_up[z,k] >= zone_reqs[z].RI_up * 3 #Fikse at 3 er DT
-            + zone_reqs[z].NI_up * sum(wp_avail[a,k] for a in areas_in_zone[z]; init=0.0)
+            + zone_reqs[z].NI_up * sum(wp_avail[a,k] for a in areas_in_zone[z]; init=0.0) + 0.03 * sum(AMData[iArea].MLData[iLoad].Load[iWeek,k]
+                 for iArea in areas_in_zone[z]
+                 for iLoad in 1:AMData[iArea].NLoad; init=0.0)
          )
          @constraint(M, reserve_req_down[z=1:NZ-1, k=1:NK],
          cap_zone_down[z,k] >= zone_reqs[z].RI_down * 3 #Fikse at 3 er DT
-            + zone_reqs[z].NI_down * sum(wp_avail[a,k] for a in areas_in_zone[z]; init=0.0)
+            + zone_reqs[z].NI_down * sum(wp_avail[a,k] for a in areas_in_zone[z]; init=0.0) + 0.03 * sum(AMData[iArea].MLData[iLoad].Load[iWeek,k]
+                 for iArea in areas_in_zone[z]
+                 for iLoad in 1:AMData[iArea].NLoad; init=0.0)
          )
          @constraint(M, reserve_split_down[z=1:NZ, k=1:NK],
          cap_zone_down[z,k] ==
