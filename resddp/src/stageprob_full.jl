040fab0869075111780edb3334a5906b9b7b46c4
kinevinningland <kine.v@me.com>
2026-04-30 09:29:59 +0200

    Describe change

--- resddp/src/stageprob_full.jl
+++ resddp/src/stageprob_full.jl
@@ -147,7 +147,6 @@ module StageProbFull
          hydrosys_to_area = ORData.hydrosys_to_area
          a = ORData.a
          b = ORData.b
-         println("Det er reserver i stageprob")
 
          #Opp- og nedreguleringsreserver
          @variable(M, 0 <= cap_zone_up[z=1:NZ, k=1:NK], base_name="cap_zone_up")
@@ -166,7 +165,7 @@ module StageProbFull
             + zone_reqs[z].NI_up * sum(wp_avail[a,k] for a in areas_in_zone[z] if !(a in zone_reqs[z].owp_areas_in_zone); init=0.0)
             + zone_reqs[z].NI_up_OWP * sum(wp_avail[a,k] for a in zone_reqs[z].owp_areas_in_zone; init=0.0)
             + (sqrt(a*zone_reqs[z].MaxLoad/(CNS.MW2GW*CTI.DT)+b^2)-b)*CNS.MW2GW*CTI.DT)
-         
+         println(zone_reqs[1].RI_up * CTI.DT,"mengde RI")
          @expression(M, cap_down_amount[z = 1:NZ, k=1:NK], 
             zone_reqs[z].RI_down * CTI.DT
             + zone_reqs[z].NI_down * sum(wp_avail[a,k] for a in areas_in_zone[z] if !(a in zone_reqs[z].owp_areas_in_zone); init=0.0)
