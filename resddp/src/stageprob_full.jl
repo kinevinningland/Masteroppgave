4f09c5c38beb58cc4ad1e70da3d0438a0933154b
kinevinningland <kine.v@me.com>
2026-05-27 13:43:05 +0200

    Describe change

--- resddp/src/stageprob_full.jl
+++ resddp/src/stageprob_full.jl
@@ -188,10 +188,12 @@ module StageProbFull
             + (sqrt(a*zone_reqs[z].MaxLoad/(CNS.MW2GW*CTI.DT)+b^2)-b)*CNS.MW2GW*CTI.DT)
 
          if t==1
-            println("Reserve requirements for stage 1 RI (GWh/step): ", zone_reqs[z].RI_down * CTI.DT)
-            println("Reserve requirements for stage 1 NI onshore (GWh/step): ", zone_reqs[z].NI_down * sum(wp_avail[a,k] for a in areas_in_zone[z] if !(a in zone_reqs[z].owp_areas_in_zone); init=0.0))
-            println("Reserve requirements for stage 1 NI offshore (GWh/step): ", zone_reqs[z].NI_down_OWP * sum(wp_avail[a,k] for a in zone_reqs[z].owp_areas_in_zone; init=0.0))
-            println("Reserve requirement for stage 1 load-based component (GWh/step): ", (sqrt(a*zone_reqs[z].MaxLoad/(CNS.MW2GW*CTI.DT)+b^2)-b)*CNS.MW2GW*CTI.DT)
+            zi = 1
+            println("Reserve requirements for stage 1 RI (GWh/step): ", zone_reqs[zi].RI_down * CTI.DT)
+            println("Reserve requirements for stage 1 NI onshore (GWh/step): ", zone_reqs[zi].NI_down * sum(wp_avail[a,k] for a in areas_in_zone[zi] if !(a in zone_reqs[zi].owp_areas_in_zone); init=0.0))
+            println("Reserve requirements for stage 1 NI offshore (GWh/step): ", zone_reqs[zi].NI_down_OWP * sum(wp_avail[a,k] for a in zone_reqs[zi].owp_areas_in_zone; init=0.0))
+            println("Reserve requirement for stage 1 load-based component (GWh/step): ", (sqrt(a*zone_reqs[zi].MaxLoad/(CNS.MW2GW*CTI.DT)+b^2)-b)*CNS.MW2GW*CTI.DT)
+            println("last:",zone_reqs[zi].MaxLoad)
          end
          
          @constraint(M, reserve_req_up[z=1:NZ, k=1:NK], cap_zone_up[z,k] >= cap_up_amount[z,k])
